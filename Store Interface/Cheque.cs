d6932d5de182b7baf3532340a6c6231b5b63aefd
Trppyxd <33310226+Trppyxd@users.noreply.github.com>
2018-09-06 09:29:45 +0300

    Changed to public

---
+++ Store Interface/Cheque.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Store_Interface
+{
+    class Cheque
+    {
+        public void Print(List<StoreItem> shoppingcart, double total)
+        {
+            foreach (var item in shoppingcart)
+            {
+                total += (item as StoreItem).Price;
+                File.WriteAllText("Cheque.txt", "Your total is: " + total.ToString() + " euro");
+                File.AppendAllText("Cheque.txt", Environment.NewLine);
+            }
+            Process.Start("Cheque.txt");
+        }
+    }
+}
--- Store Interface/MainWindow.xaml.cs
+++ Store Interface/MainWindow.xaml.cs
@@ -1,28 +1,169 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Store_Interface
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public List<StoreItem> list;
+        public List<StoreItem> shoppingcart;
+        public string fPath = "Tekst.txt";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            if (!File.Exists("Tekst.txt"))
+            {
+                using (var myFile = File.Create("Tekst.txt"))
+                {
+
+                }
+
+            }
+            openFile();
+            saveFile();
+            list = new List<StoreItem>();
+            shoppingcart = new List<StoreItem>();
+        }
+
+        private void btnAddToShoppingCart(object sender, RoutedEventArgs e)
+        {
+            if (listview_StoreItems.SelectedItem != null)
+            {
+                if ((listview_StoreItems.SelectedItem as StoreItem).Amount <= 0)
+                {
+                    MessageBox.Show("Sorry, that item isnt in stock.");
+                    listview_StoreItems.Items.Remove(listview_StoreItems.SelectedItem);
+                    saveFile();
+                    listview_StoreItems.Items.Clear();
+                    openFile();
+
+                }
+                else
+                {
+                    listview_ShoppingCart.Items.Add(new StoreItem
+                    {
+                        Name = (listview_StoreItems.SelectedItem as StoreItem).Name,
+                        Price = (listview_StoreItems.SelectedItem as StoreItem).Price,
+                        Amount = 1
+                    });
+                    shoppingcart.Add(new StoreItem
+                    {
+                        Name = (listview_StoreItems.SelectedItem as StoreItem).Name,
+                        Price = (listview_StoreItems.SelectedItem as StoreItem).Price,
+                        Amount = 1
+                    });
+                }
+                foreach (StoreItem item in listview_StoreItems.Items)
+                {
+                    if (item.Name == (listview_StoreItems.SelectedItem as StoreItem).Name)
+                    {
+                        item.Amount--;
+                        saveFile();
+                        listview_StoreItems.Items.Clear();
+                        openFile();
+                        return;
+                    }
+                }
+
+            }
+        }
+
+        private void btnBuy_Click(object sender, RoutedEventArgs e)
+        {
+            if (!listview_ShoppingCart.HasItems)
+            {
+                MessageBox.Show("You need to select an item!");
+            }
+            else
+            {
+                var cheque = new Cheque();
+                double total = 0;
+                cheque.Print(shoppingcart, total);
+            }
+        }
+
+        private void btnAddItem(object sender, RoutedEventArgs e)
+        {
+            bool cont = true;
+            if (txt_Price.Text != "")
+            {
+                foreach (StoreItem cItem in listview_StoreItems.Items)
+                {
+                    if (cItem.Name == txt_Name.Text && cItem.Price == double.Parse(((txt_Price.Text)), System.Globalization.CultureInfo.InvariantCulture))
+                    {
+                        cItem.Amount++;
+                        cont = false;
+                    }
+                }
+                if (cont == true)
+                {
+                    listview_StoreItems.Items.Add(new StoreItem
+                    {
+                        Name = (txt_Name.Text).ToString(),
+                        Price = double.Parse(((txt_Price.Text)), System.Globalization.CultureInfo.InvariantCulture),
+                        Amount = 1
+                    });
+                }
+                saveFile();
+                listview_StoreItems.Items.Clear();
+                openFile();
+            }
+        }
+
+        private void btnRemoveItem(object sender, RoutedEventArgs e)
+        {
+            listview_ShoppingCart.Items.Remove(listview_ShoppingCart.Items);
+            shoppingcart = new List<StoreItem>();
+            Refresh();
+        }
+
+        public void openFile()
+        {
+            using (var file = File.OpenText(fPath))
+            {
+                while (!file.EndOfStream)
+                {
+                    var line = file.ReadLine();
+                    var line2 = line.Split(';').ToList();
+                    listview_StoreItems.Items.Add(new StoreItem
+                    {
+                        Name = Convert.ToString(line2[0]),
+                        Price = double.Parse((line2[1])),
+                        Amount = Int32.Parse(line2[2])
+                    });
+                }
+            }
+        }
+
+        public void saveFile()
+        {
+            using (StreamWriter file = new StreamWriter(fPath, false))
+            {
+                foreach (StoreItem item in listview_StoreItems.Items)
+                {
+                    file.WriteLine(item.Name + ";" + item.Price + ";" + item.Amount);
+                }
+            }
+        }
+
+        public void Refresh()
+        {
+            listview_ShoppingCart.Items.Clear();
+            saveFile();
+            listview_StoreItems.Items.Clear();
+            openFile();
         }
     }
+}
+public class StoreItem
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Amount { get; set; }
 }
